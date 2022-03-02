cedc77a5a8f1e064b692b46533877a52789db840
SCFloki <kevin.doublet02@gmail.com>
2022-03-02 02:27:12 +0100

    Bootstrap/router/Navigation init

--- src/App.vue
+++ src/App.vue
@@ -1,107 +1,49 @@
 <template>
-	<div id="app" class="app">
-		<h1>{{ restaurantName }}</h1>
-		<p class="description">
-			Bienvenue dans notre café {{ restaurantName }}! Nous sommes réputés pour
-			notre pain et nos merveilleuses pâtisseries. Faites vous plaisir dès le
-			matin ou avec un goûter réconfortant. Mais attention, vous verrez qu'il
-			est difficile de s'arrêter.
-		</p>
-
-		<section class="menu">
-			<h2>Menu</h2>
-			<MenuItem
-				v-for="item in simpleMenu"
-				:addToShoppingCart="addToShoppingCart"
-				:name="item.name"
-				:image="item.image"
-				:quantity="item.quantity"
-				:inStock="item.inStock"
-				:key="item.name"
-			/>
-		</section>
-
-		<aside class="shopping-cart">
-			<h2>Panier d'achat : {{ shoppingCart }} articles</h2>
-		</aside>
-
-		<h2>Contactez nous</h2>
-		<p>Adresse : {{ address }}</p>
-		<p>Téléphone : {{ phone }}</p>
-		<p>Email : {{ email }}</p>
-		<p>Horaires :</p>
-		<ul>
-			<li>L-V: 06:00 à 16:00</li>
-			<li>Samedi: 07:00 à 14:00</li>
-			<li>Dimanche: 07:00 à 12:00</li>
-		</ul>
-		<footer class="footer">
-			<p>{{ copyright }}</p>
-		</footer>
+	<div id="app">
+  <b-navbar toggleable="lg" type="dark" id="navbar">
+  <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-navbar-nav>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-nav-item><router-link to="/">Projects</router-link></b-nav-item>
+        <b-navbar-brand id="navbar-brand"><router-link to="/">SCFloki</router-link></b-navbar-brand>
+        <b-nav-item><router-link to="/about">About</router-link></b-nav-item>
+      </b-collapse>
+    </b-navbar-nav>
+    <router-view/>
+  </b-navbar>
 	</div>
 </template>
 
 <script>
-import MenuItem from "./components/MenuItem"
-export default {
-	name: "App",
-	components: {
-		MenuItem
-	},
-	data() {
-		return {
-			address: "18 avenue du Beurre, Paris, France",
-			email: "hello@cafewithavue.bakery",
-			phone: "01 88 88 88 88",
-			restaurantName: "La belle vue",
-			shoppingCart: 0,
-			simpleMenu: [
-				{
-					name: "Croissant",
-					image: {
-						source: require("@/assets/images/croissant.jpg"),
-						alt: "Un croissant"
-					},
-					inStock: true,
-					quantity: 1
-				},
-				{
-					name: "Baguette de pain",
-					image: {
-						source: require("@/assets/images/french-baguette.jpeg"),
-						alt: "Quatre baguettes de pain"
-					},
-					inStock: true,
-					quantity: 1
-				},
-				{
-					name: "Éclair",
-					image: {
-						source: require("@/assets/images/eclair.jpg"),
-						alt: "Éclair au chocolat"
-					},
-					inStock: false,
-					quantity: 1
-				}
-			]
-		}
-	},
-	computed: {
-		copyright() {
-			const currentYear = new Date().getFullYear()
-			return `Copyright ${this.restaurantName} ${currentYear}`
-		}
-	},
-	methods: {
-		addToShoppingCart(amount) {
-			this.shoppingCart += amount
-		}
-	}
-}
 </script>
 
 <style lang="scss">
+
+html, body {
+  background-color: black;
+};
+
+#navbar ul{
+  margin:auto;
+}
+
+#navbar a{
+  text-decoration: none;
+  color: white;
+  letter-spacing: 8px;
+}
+
+#navbar-brand {
+  margin-right: 180px;
+  margin-left: 180px;
+  font-weight: bold
+}
+
+
+
+
 #app {
+  background-color: black;
 	font-family: "Avenir", Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 
 Vue.config.productionTip = false
 
+// plugins
+import BootstrapVue from 'bootstrap-vue'
+Vue.use(BootstrapVue);
+
+// bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
